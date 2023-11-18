b88c7439bec78a38691ebda26ef2ecac8b2b880e
VladLore <vladiknikishin@gmail.com>
2023-11-18 15:13:48 +0300

    Решение HW5

--- HOMEWORK/HW5/user.py
+++ HOMEWORK/HW5/user.py
@@ -9,45 +9,50 @@ users = [
 ]
 
 
-@router.get('/')
+@router.get("/")
 async def root():
     return "Hello world"
 
-@router.get('/user/', response_model=list[User])
+
+@router.get("/user/", response_model=list[User])
 async def get_users():
     return users
 
-@router.get('/user/{user_id}', response_model=User)
+
+@router.get("/user/{user_id}", response_model=User)
 async def one_user(user_id: int):
     if len(users) < user_id:
-        raise HTTPException(status_code=404, detail='User not Found')
-    return users[user_id-1]
+        raise HTTPException(status_code=404, detail="User not Found")
+    return users[user_id - 1]
 
-@router.post('/users/', response_model=list[User])
+
+@router.post("/users/", response_model=list[User])
 async def new_user(new_user: UserIn):
     new_user = User(
-        id=len(users)+1,
+        id=len(users) + 1,
         name=new_user.name,
         email=new_user.email,
-        password=new_user.password
-        )
+        password=new_user.password,
+    )
     users.append(new_user)
     return users
 
-@router.put('/user/{user_id}', response_model= list[User])
+
+@router.put("/user/{user_id}", response_model=list[User])
 async def edit_user(user_id: int, edit_user: UserIn):
     for user in users:
         if user.id == user_id:
-            user.name=edit_user.name
-            user.email=edit_user.email
-            user.password=edit_user.password
+            user.name = edit_user.name
+            user.email = edit_user.email
+            user.password = edit_user.password
             return users
-    raise HTTPException(status_code=404, detail='User not found')
+    raise HTTPException(status_code=404, detail="User not found")
 
-@router.delete('/user/{user_id}')
+
+@router.delete("/user/{user_id}")
 async def del_user(user_id: int):
     for user in users:
         if user.id == user_id:
             users.remove(user)
             return f"Пользователь удален"
-    raise HTTPException(status_code=404, detail='User not found')
+    raise HTTPException(status_code=404, detail="User not found")
